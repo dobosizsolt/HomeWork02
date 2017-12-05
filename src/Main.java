86d82bb6095fd9a08c0ec2cd15422ec8033456c2
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-05 18:38:27 +0100

    #1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import hu.helixlab.CalculatorService;
+
 public class Main {
 
+    //Önellenőrzés végett készítettem.
+
     public static void main(String[] args) {
-	
+
+        System.out.println(CalculatorService.isItEven(7));
+        System.out.println(CalculatorService.isItEven(8));
+
     }
 }
